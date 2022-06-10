bd9d08a428eaefd095a64c33a6341d20aef51ced
Aaron Brandenberger <brmaaron@gmail.com>
2022-06-09 19:48:38 -0600

    Create division function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function add(a, b) {
-    return a + b;
-}
+let add = (a, b) => a + b;
 
-function subtract(a, b) {
-    return a - b;
-}
+let subtract = (a, b) => a - b;
+
+let divide = (a, b) => a / b;
+
+let multiply = (a, b) => a * b;
