bd46bd5bc6f0a6a5a37f7171803b82899d3d3fb6
Aaron Brandenberger <brmaaron@gmail.com>
2022-06-09 20:22:12 -0600

    Create function to accept operator/operands  and evaluate relevant operating function

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+': return add(a, b);
+            break;
+
+        case '-': return subtract(a, b);
+            break;
+
+        case '/': return divide(a, b);
+            break;
+
+        case '*': return multiply(a, b);
+            break;
+    }
+}
 
 
 let add = (a, b) => a + b;
@@ -7,3 +22,5 @@ let subtract = (a, b) => a - b;
 let divide = (a, b) => a / b;
 
 let multiply = (a, b) => a * b;
+
+operate(6, 4, '+');
