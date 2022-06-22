822648a61e5d4afb5dc6ce15c2a5a0bb75573d59
Aaron Brandenberger <brmaaron@gmail.com>
2022-06-22 16:01:03 -0600

    Add comments and improve script readability

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+/* Divs */
 const calDiv = document.createElement('div');
 const displayDiv = document.createElement('div');
 const display = document.createElement('display');
 const btnDiv = document.createElement('div');
 const numsDiv = document.createElement('div');
 const operatorsDiv = document.createElement('div');
+
 const btns = document.createElement('div');
+
+/* Operator Buttons */
 const addBtn = document.createElement('div');
 const subBtn = document.createElement('div');
 const multBtn = document.createElement('div');
 const divBtn = document.createElement('div');
+
+/* Number Buttons */
 const zeroBtn = document.createElement('div');
 const oneBtn = document.createElement('div');
 const twoBtn = document.createElement('div');
@@ -19,21 +25,31 @@ const sixBtn = document.createElement('div');
 const sevenBtn = document.createElement('div');
 const eightBtn = document.createElement('div');
 const nineBtn = document.createElement('div');
+
+/* Clear and Equals Function Buttons */
 const acBtn = document.createElement('div');
 const equalBtn = document.createElement('div');
+
+/* Body and Page Selectors */
 const body = document.querySelector('#bod');
 const page = document.querySelector('#pg');
 
+/* Div Selectors */
+const calDivs = document.querySelectorAll('.calDivs');
+
+/* HTML IDs */
 calDiv.setAttribute('id', 'calBox');
 displayDiv.setAttribute('id', 'dispDiv');
 btnDiv.setAttribute('id', 'btnBox');
 display.setAttribute('id', 'disp');
 
+/* HTML Classes */
 btns.classList.add('btn');
 calDiv.classList.add('calDivs');
 displayDiv.classList.add('calDivs');
 btnDiv.classList.add('calDivs');
 
+/* Page and Body Styles */
 page.style.height = '100%';
 body.style.height = '100%';
 body.style.width = '100%';
@@ -43,14 +59,15 @@ body.style.alignItems = 'center';
 body.style.flexDirection = 'column';
 
 
-const calDivs = document.querySelectorAll('.calDivs');
 
+/* calDivs Class Styling */
 calDivs.forEach(calDiv => {
     calDiv.style.display = 'flex';
     calDiv.style.alignItems = 'center';
     calDiv.style.justifyContent = 'center';
 });
 
+/* Div Styling */
 calDiv.style.height = '500px';
 calDiv.style.width = '350px';
 calDiv.style.display = 'flex';
@@ -61,7 +78,6 @@ displayDiv.style.borderStyle = 'solid';
 displayDiv.style.display = 'flex';
 displayDiv.style.alignItems = 'center';
 displayDiv.style.justifyContent = 'center';
-
 displayDiv.style.padding = '0px 25px 0px 25px';
 
 display.style.height = '90px';
@@ -69,7 +85,6 @@ display.style.width = '325px';
 display.style.borderStyle = 'solid';
 display.style.flexGrow = '0';
 display.style.flexShrink = '1';
-
 
 btnDiv.style.flexGrow = '7';
 btnDiv.style.borderStyle = 'solid';
@@ -88,13 +103,7 @@ operatorsDiv.style.margin = '25px 25px 25px 0px'
 operatorsDiv.style.display = 'flex';
 operatorsDiv.style.flexWrap = 'true';
 
-
-
-
-
-
-
-
+/* HTML Layout */
 body.appendChild(calDiv);
 calDiv.appendChild(displayDiv);
 calDiv.appendChild(btnDiv);
@@ -106,7 +115,7 @@ btnBox.appendChild(operatorsDiv);
 
 
 
-
+/* Operator Selection Function for Calculator */
 function operate(a, b, operator) {
     switch (operator) {
         case '+': return add(a, b);
@@ -121,15 +130,10 @@ function operate(a, b, operator) {
         case '*': return multiply(a, b);
             break;
     }
-}
+};
 
-
+/* Operator Functions */
 let add = (a, b) => a + b;
-
 let subtract = (a, b) => a - b;
-
 let divide = (a, b) => a / b;
-
 let multiply = (a, b) => a * b;
-
-operate(6, 4, '+');
